83202f48027f7aba3b4c467abd68dba1abf4395a
syanhulinux <siyan.dev@gmail.com>
2024-03-14 15:04:24 +0800

    update evaluate code

--- single_evaluate.sh
+++ single_evaluate.sh
@@ -1,18 +1,19 @@
-scene='scene_stairs/test_full_byorder_59'
-model='scene_stairs/train_full_byorder_85'
-exp_name='output_sparse_setting_sc0'
-exp_ts='2024-03-10_22\:02\:10'
+scene='scene_chess/test_full_byorder_59'
+model='scene_chess/train_full_byorder_85'
+exp_name='output_lod0'
+exp_ts='2024-03-14_13:53:35'
 ratio=1
 
-# ./evaluate.sh -d ${scene} \
-# -m ${model} \
-# -l ${exp_name/exp_ts} \
-# --gpu 0 \
-# --r ${ratio}
+echo "from " + "$PWD"/outputs/${model}/${exp_name}/${exp_ts}
 
-# cp -r outputs/scene_stairs/train_full_byorder_85/output_sparse_setting_sc0/2024-03-10_22\:02\:10 \
-# data/scene_stairs/train_full_byorder_85/output_sparse_setting_sc0
+echo "to " + "$PWD"/data/${model}/${exp_name}
 
-# python evaluate.py -s data/scene_stairs/test_full_byorder_59 -m outputs/scene_stairs/train_full_byorder_85/output_sparse_setting_sc0/2024-03-10_22\:02\:10
-python evaluate_dof.py -s data/scene_stairs/test_full_byorder_59 -m data/scene_stairs/train_full_byorder_85/current
-# python evaluate_dof.py -s data/scene_stairs/train_full_byorder_85 -m data/scene_stairs/train_full_byorder_85/current
+cp -r "$PWD"/outputs/${model}/${exp_name}/${exp_ts} "$PWD"/data/${model}/${exp_ts}
+
+mv "$PWD"/data/${model}/${exp_ts} "$PWD"/data/${model}/${exp_name}
+
+rm -rf data/${model}/${exp_name}/test
+
+python evaluate_dof.py -s data/${scene} -m data/${model}/${exp_name}
+
+python evaluate_dof.py -s data/${model} -m data/${model}/${exp_name}
